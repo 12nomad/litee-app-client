67869b19e5a8cbf72dc48ac349f16d69179d7e92
Jo Harijaonina <jo.harijaonina@gmail.com>
2023-08-17 10:06:19 +0300

    fixed cookie

--- src/store/features/api.slice.ts
+++ src/store/features/api.slice.ts
@@ -73,6 +73,7 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_SERVER_URL,
     credentials: 'include',
+    mode: 'cors',
   }),
   endpoints: (builder) => ({
     // TODO: user
