5f38257c5d4a0f5f478f0d9549c9e7a84d9a114c
Jo Harijaonina <jo.harijaonina@gmail.com>
2023-09-29 11:08:37 +0300

    fix Guard making request before cookie was set on production

--- src/screens/home/Guard.tsx
+++ src/screens/home/Guard.tsx
@@ -1,32 +1,32 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import Loading from "../../components/ui/Loading";
 import { socket, useGetAuthUserQuery } from "../../store/features/api.slice";
 import { useAppDispatch } from "../../store/store";
 import { addUser } from "../../store/features/user.slice";
 import { EVENTS } from "../../data/event.constant";
+import Loading from "../../components/ui/Loading";
 
 const Guard = () => {
-  const { isLoading, data, error } = useGetAuthUserQuery();
+  const { isLoading, data, error, refetch } = useGetAuthUserQuery();
+  const [errorCount, setErrorCount] = useState(0);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const delay = setTimeout(() => {
-      if (!isLoading && data) {
-        dispatch(addUser({ user: data }));
-        sessionStorage.setItem("lt-app-key", data.username);
-        socket.connect();
-        socket.emit<`${EVENTS}`>("CONNECT", { username: data.username });
-      } else if (!isLoading && error) {
-        return navigate("/auth", { replace: true });
+    if (!isLoading && data) {
+      dispatch(addUser({ user: data }));
+      sessionStorage.setItem("lt-app-key", data.username);
+      socket.connect();
+      socket.emit<`${EVENTS}`>("CONNECT", { username: data.username });
+    } else if (!isLoading && error) {
+      if (errorCount === 0) {
+        setErrorCount((prev) => ++prev);
+        refetch();
+      } else {
+        navigate("/auth", { replace: true });
       }
-    }, 2500);
-
-    return () => {
-      clearTimeout(delay);
-    };
+    }
   }, [error, data]);
 
   if (isLoading) return <Loading withNav={false} />;
